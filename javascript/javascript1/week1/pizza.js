ed903820e6dbcc608824a5af3a27d8d468ceae18
pattnaikupasana <70476038+pattnaikupasana@users.noreply.github.com>
2021-05-05 19:21:32 +0200

    added pizza file

---
+++ javascript/javascript1/week1/pizza.js
@@ -0,0 +1,16 @@
+console.log("I love pizza");
+let favouritePizza = "Napoletana";
+let pricePizza = 200;
+console.log(`New pizza order, \nFavourite Pizza: ${favouritePizza} \nThe price of the pizza is: ${pricePizza}`);
+let amountPizza = 2;
+//let amountPizza = prompt("How mamy pizza you want ?");
+let familyPizza = [true, false]
+let totalPriceofPizza
+if (familyPizza = true) {
+
+    totalPriceofPizza = 2 * pricePizza * amountPizza;
+} else {
+
+    totalPriceofPizza = pricePizza * amountPizza;
+}
+console.log(`No of pizza order:${amountPizza} \nFamily pizza: ${familyPizza}\nPizza name: ${favouritePizza}. \nTotal cost for the order is: ${totalPriceofPizza}`)
