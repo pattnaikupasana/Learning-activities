cf11153b1ed89f5d5e34e0803a535b1adeb96f47
pattnaikupasana <70476038+pattnaikupasana@users.noreply.github.com>
2021-05-05 19:17:34 +0200

    added Age-ify file

---
+++ javascript/javascript1/week1/Age-ify.js
@@ -0,0 +1,10 @@
+let yearofBirth = 1987;
+let yearFuture = 2027;
+let age = yearFuture - yearofBirth;
+console.log(`You will be ${age} years old in ${yearFuture}`)
+
+/*let yearofBirth, yearFuture, age;
+yearofBirth = prompt("Enter birth year");
+yearFuture = prompt("Enter the year you want to calculate");
+age = yearFuture - yearofBirth;
+console.log(`You will be ${age} years old in ${yearFuture}`)*/
