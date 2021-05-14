9e5a81af43095f42ce43a213f3c63c8d46c083e0
pattnaikupasana <70476038+pattnaikupasana@users.noreply.github.com>
2021-05-14 23:13:59 +0200

    added weatherWear file

---
+++ javascript/javascript1/week2/weatherWear.js
@@ -0,0 +1,20 @@
+function weatherWear(temp) {
+    if (temp <= 5) {
+        return 'Thick jacket and hand gloves';
+    }
+    if (temp < 10) {
+        return 'Normal jacket and gloves';
+    }
+    if (temp < 15) {
+        return 'Thin jacket and wear anythign you want ';
+    }
+    if (temp < 20) {
+        return 'Shirt,t-shirt,jeans,etc';
+    }
+    if (temp < 25) {
+        return 'Shorts and t-shirt';
+    }
+    if (temp >= 25) {
+        return 'Nude on beach';
+    }
+}
