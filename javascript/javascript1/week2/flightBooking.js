ed3092638561a3cec1a36b22ae4a05098de9fe2e
pattnaikupasana <70476038+pattnaikupasana@users.noreply.github.com>
2021-05-20 19:31:33 +0200

    rectified flightBookingn file

--- javascript/javascript1/week2/flightBooking.js
+++ javascript/javascript1/week2/flightBooking.js
@@ -2,12 +2,12 @@ function getFullName(firstName, surName, useFormalName, gender) {
 
     if (useFormalName) {
         if (gender == "male") {
-            return (`Lord ${firstName} ${surName}`);
+            return `Lord ${firstName} ${surName}`
         } else {
-            return (`Lady ${firstName} ${surName}`);
+            return `Lady ${firstName} ${surName}`
         }
 
     } else {
-        return (`${firstName} ${surName}`);
+        return `${firstName} ${surName}`
     }
 }
