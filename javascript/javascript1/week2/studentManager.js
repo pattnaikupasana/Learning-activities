a6d4080084c1abcff8f167ab75d30009de5af285
pattnaikupasana <70476038+pattnaikupasana@users.noreply.github.com>
2021-05-20 19:32:02 +0200

    rectified studentManager file

--- javascript/javascript1/week2/studentManager.js
+++ javascript/javascript1/week2/studentManager.js
@@ -1,17 +1,20 @@
 let class07Students = []
 
 function addStudentToClass(studentName, isQueen) {
-    const n = (isQueen === true) ? 6 : 5;
+    const totalStudent = (isQueen === true) ? 6 : 5;
     if (isQueen == 'true') {
         class07Students.push(studentName)
-    } else if (class07Students.includes(studentName)) {
-        return (`Student ${studentName} is already in class`)
-    } else if (class07Students.length > n) {
-        return ('Class is full.')
-    } else if (studentName == '' || studentName.length < 2) {
-        return ('You need add something.')
+    }
+    if (class07Students.includes(studentName)) {
+        return `Student ${studentName} is already in class`
+    }
+    if (class07Students.length > totalStudent) {
+        return 'Class is full'
+    }
+    if (studentName == '' || studentName.length < 2) {
+        return 'You need add something'
     } else {
         class07Students.push(studentName)
-        return ('Welcome to class')
+        return 'Welcome to class'
     }
 }
