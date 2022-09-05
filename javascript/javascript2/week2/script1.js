51143332d90c1460a36a80925b412c29fb904bcc
pattnaikupasana <70476038+pattnaikupasana@users.noreply.github.com>
2022-09-05 07:36:29 +0200

    delete

--- javascript/javascript2/week2/script1.js
+++
@@ -1,4 +0,0 @@
-/*Doubling of number */
-let numbers = [1, 2, 3, 4];
-let newNumbersDouble = numbers.filter(a => a % 2 !== 0).map(a => a * 2);
-console.log("The doubled numbers are", newNumbersDouble);
